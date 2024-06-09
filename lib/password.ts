1ec73c6de0d7b3fe9f7dc36327f3cc869db8c42a
Tiffany Tang <tiffanytangt@users.noreply.github.com>
2024-06-09 02:08:14 -0700

    run prettier

--- lib/password.ts
+++ lib/password.ts
@@ -1,14 +1,14 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
 
 export const saltAndHash = async (password: string) => {
-    const salt = await bcrypt.genSalt(saltRounds);
-    console.log(password)
-    return bcrypt.hash(password, salt);
-}
+  const salt = await bcrypt.genSalt(saltRounds);
+  console.log(password);
+  return bcrypt.hash(password, salt);
+};
 
 export const verify = async (password: string, hashedPassword: string) => {
-    const result = await bcrypt.compare(password, hashedPassword);
+  const result = await bcrypt.compare(password, hashedPassword);
 
-    if (!result) throw new Error(`Incorrect password`);
-}
+  if (!result) throw new Error(`Incorrect password`);
+};
