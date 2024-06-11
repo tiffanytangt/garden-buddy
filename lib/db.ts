1dd96df7e4967967f16f34be66ff848dda373747
tiffanytangt <12769383+tiffanytangt@users.noreply.github.com>
2024-06-11 06:15:27 -0700

    fix dev too many db conns

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-const db = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+// see https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices#problem
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const db = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default db;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = db;
