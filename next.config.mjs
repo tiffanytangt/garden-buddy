215a529220b363f94fc21fff4a326e3bfd342f1f
tiffanytangt <12769383+tiffanytangt@users.noreply.github.com>
2024-06-30 05:23:39 -0700

    fix: allow s3 hosted images to be served

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["localhost", "127.0.0.1", 't-garden-buddy.s3.us-west-2.amazonaws.com'],
+    },
+};
 
 export default nextConfig;
