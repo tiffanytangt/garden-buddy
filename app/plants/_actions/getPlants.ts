e408b9625bbae935042919173c213a47617c8948
tiffanytangt <12769383+tiffanytangt@users.noreply.github.com>
2025-02-18 00:28:24 -0800

    feat: Add limit option to getPlants function

--- app/plants/_actions/getPlants.ts
+++ app/plants/_actions/getPlants.ts
@@ -6,15 +6,17 @@ import Prisma from '@prisma/client';
 
 interface getPlantOptions {
   sortBy?: keyof Prisma.Prisma.PlantOrderByWithRelationInput;
+  limit?: number;
 }
 export const getPlants = cache(
-  (userId: string, { sortBy: orderBy }: getPlantOptions) => {
+  (userId: string, { sortBy: orderBy, limit: take }: getPlantOptions) => {
     return db.plant.findMany({
       where: { userId: userId },
       include: { photo: true },
       orderBy: {
         ...(orderBy && { [orderBy]: 'desc' }),
       },
+      take,
     });
   }
 );
